12fa040149f9a48680538a5c9428bf77e671d62a
Tsan Tri Duc <tsantriduc@gmail.com>
2026-05-30 21:20:03 +0700

    Call API get me profile

---
+++ queries/useAccount.tsx
@@ -0,0 +1,9 @@
+import accountApiRequest from "@/app/apiRequest/account";
+import { useQuery } from "@tanstack/react-query";
+
+export const useAccountProfile = () => {
+  return useQuery({
+    queryKey: ["account-profile"],
+    queryFn: accountApiRequest.me,
+  });
+};
