082a1a5a91873f3100cf6c1e86b80fb0d551180d
Tsan Tri Duc <tsantriduc@gmail.com>
2026-06-04 04:18:08 +0700

    Call API add table

--- app/manage/tables/add-table.tsx
+++ app/manage/tables/add-table.tsx
@@ -21,7 +21,7 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import { getVietnameseTableStatus } from "@/lib/utils";
+import { getVietnameseTableStatus, handleErrorApi } from "@/lib/utils";
 import {
   CreateTableBody,
   CreateTableBodyType,
@@ -34,9 +34,14 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useAddTableMutation } from "@/queries/useTable";
+import { toast } from "sonner";
 
 export default function AddTable() {
   const [open, setOpen] = useState(false);
+
+  const addTableMutation = useAddTableMutation();
+
   const form = useForm<CreateTableBodyType>({
     resolver: zodResolver(CreateTableBody) as any,
     defaultValues: {
@@ -45,8 +50,33 @@ export default function AddTable() {
       status: TableStatus.Hidden,
     },
   });
+
+  const onSubmit = async (values: CreateTableBodyType) => {
+    if (addTableMutation.isPending) return;
+    try {
+      const result = await addTableMutation.mutateAsync(values);
+      toast.success(result.payload.message);
+      reset();
+      setOpen(false);
+    } catch (error) {
+      handleErrorApi({ error, setError: form.setError });
+    }
+  };
+
+  const reset = () => {
+    form.reset();
+  };
+
   return (
-    <Dialog onOpenChange={setOpen} open={open}>
+    <Dialog
+      onOpenChange={(value) => {
+        if (!value) {
+          reset();
+        }
+        setOpen(value);
+      }}
+      open={open}
+    >
       <DialogTrigger asChild>
         <Button size="sm" className="h-7 gap-1">
           <PlusCircle className="h-3.5 w-3.5" />
@@ -67,6 +97,10 @@ export default function AddTable() {
             noValidate
             className="grid auto-rows-max items-start gap-4 md:gap-8"
             id="add-table-form"
+            onSubmit={form.handleSubmit(onSubmit, (e) => {
+              console.log(e);
+            })}
+            onReset={reset}
           >
             <div className="grid gap-4 py-4">
               <FormField
